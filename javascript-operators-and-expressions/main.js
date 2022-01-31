bef2dc9aca61d77a06bd83474f94239ca43ede2d
Rosemary Liu <rliu18@gmail.com>
2022-01-31 11:43:46 -0800

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 10;
+var height = 20;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof:', typeof area);
+
+var bill = 5;
+var payment = 3;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 80;
+var final = 70;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Rosemary';
+var lastName = 'Liu';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 7;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 100;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
