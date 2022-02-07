514a2ec0c1b202d2f6e54f7fb256bae9d8ddbc0a
Rosemary Liu <rliu18@gmail.com>
2022-02-07 10:31:42 -0800

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+var $heading = document.querySelector('h1');
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+console.dir($hint);
+
+var $pElements = document.querySelectorAll('p');
+console.dir($pElements);
+
+var $link = document.querySelectorAll('.example-link');
+console.dir($link);
