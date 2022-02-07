e613ef2e2352787d9043b8b091eda2668f442aef
Rosemary Liu <rliu18@gmail.com>
2022-02-07 10:47:10 -0800

    Added console logs.

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,14 +1,21 @@
+console.log('hello, world');
+
 var $heading = document.querySelector('h1');
+console.log($heading);
 console.dir($heading);
 
 var $explanation = document.querySelector('#explanation');
+console.log($explanation);
 console.dir($explanation);
 
 var $hint = document.querySelector('.hint');
+console.log($hint);
 console.dir($hint);
 
 var $pElements = document.querySelectorAll('p');
+console.log($pElements);
 console.dir($pElements);
 
 var $link = document.querySelectorAll('.example-link');
+console.log($link);
 console.dir($link);
