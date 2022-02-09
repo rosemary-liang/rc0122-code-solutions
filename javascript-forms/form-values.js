3a920b795e8bb8429bf6ee34ce7aeae82ea8a9e9
Rosemary Liu <rliu18@gmail.com>
2022-02-09 11:35:29 -0800

    Completed javascript-forms exercise.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,12 @@
+var $submit = document.querySelector('#contact-form');
+$submit.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  var submittedValues = {};
+
+  submittedValues.name = $submit.elements.name.value;
+  submittedValues.email = $submit.elements.email.value;
+  submittedValues.message = $submit.elements.message.value;
+
+  console.log(submittedValues);
+});
