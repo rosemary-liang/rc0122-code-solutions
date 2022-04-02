05c91c9f6447b8b533089c2c6d17f3cadf40fc8f
Rosemary Liu <rliu18@gmail.com>
2022-04-02 21:55:19 +0000

    Updated html to make the circle a button

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -1,2 +1,4 @@
 // import ReactDOM from 'react-dom';
 // import Stopwatch from './stopwatch';
+
+// ReactDOM.render(Stopwatch, document.querySelector('#root'));
--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+// import React from 'react';
 
-class Stopwatch extends React.Component {
+// class Stopwatch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       timer: 0,
+//       clicked: false,
+//       iconClass: 'fa-solid fa-play'
+//     };
+//   }
 
-}
+//   handleClick() {
 
-export default Stopwatch;
+//   }
+//   // tick() {
+//   //   this.setState({
+//   //     timer: {timer++}
+//   //   })
+//   // }
+
+//   render() {
+
+//   }
+// }
+
+// export default Stopwatch;
