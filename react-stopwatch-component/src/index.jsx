23a50d948c3ae612a6f503618be41ef65e6b8361
Rosemary Liu <rliu18@gmail.com>
2022-04-03 00:23:30 +0000

    First commit of stopwwatch.jsx.

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -1,4 +1,5 @@
-// import ReactDOM from 'react-dom';
-// import Stopwatch from './stopwatch';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stopwatch from './stopwatch.jsx';
 
-// ReactDOM.render(Stopwatch, document.querySelector('#root'));
+ReactDOM.render(<Stopwatch />, document.querySelector('#root'));
--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,27 +1,69 @@
-// import React from 'react';
+import React from 'react';
+class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 0,
+      isClicked: false,
+      iconClass: 'fa-solid fa-play'
+    };
 
-// class Stopwatch extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       timer: 0,
-//       clicked: false,
-//       iconClass: 'fa-solid fa-play'
-//     };
-//   }
+    this.handlePlayClick = this.handlePlayClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+  }
 
-//   handleClick() {
+  tick() {
+    let seconds = this.state.timer;
+    this.setState({ timer: seconds++ });
+  }
 
-//   }
-//   // tick() {
-//   //   this.setState({
-//   //     timer: {timer++}
-//   //   })
-//   // }
+  handleClass() {
+    if (this.state.isClicked) {
+      this.setState({ iconClass: 'fa-solid fa-pause' });
+    } else {
+      this.setState({ iconClass: 'fa-solid-fa-play' });
+    }
 
-//   render() {
+  }
 
-//   }
-// }
+  handleInterval() {
+    if (this.state.isClicked) {
+      this.intervalID = setInterval(() => this.tick, 1000);
+    } else {
+      clearInterval(this.intervalID);
+    }
+  }
 
-// export default Stopwatch;
+  handlePlayClick() {
+    this.setState({ isClicked: !this.state.isClicked });
+    this.handleClass();
+    this.handleInterval();
+  }
+
+  handleResetClick() {
+    if (!this.state.isClicked) {
+      this.setState({ timer: 0 });
+    }
+  }
+
+  render() {
+    // console.log('state:', this.state);
+    const { timer, iconClass } = this.state;
+
+    return (
+      <div className='container'>
+        <div className='row'>
+          <p>{timer}</p>
+          <button onClick={this.handlePlayClick} className='circle'></button>
+        </div>
+        <div className='row'>
+          <button onClick={this.handleResetClick}>
+            <i className={iconClass}></i>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Stopwatch;
