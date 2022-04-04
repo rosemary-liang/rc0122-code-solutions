335740fed1f29f081dc4dd7187bd862c6628f9dc
Rosemary Liu <rliu18@gmail.com>
2022-04-04 18:11:38 +0000

    Set up html

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// class ValidatedInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       icon: 'x',
+//       errorMsg: 'A password is required'
+
+//     };
+//   }
+
+//   render() {
+
+//   }
+// }
+
+// export default ValidatedInput;
