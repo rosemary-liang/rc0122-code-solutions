880b345f1212e62bb1e7e81b0e809239a25a1f84
Rosemary Liu <rliu18@gmail.com>
2022-04-04 18:42:37 +0000

    Completed react-validated-input-component challenge.

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,18 +1,51 @@
-// import React from 'react';
+import React from 'react';
 
-// class ValidatedInput extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       icon: 'x',
-//       errorMsg: 'A password is required'
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      icon: 'fa-solid fa-x',
+      errorMsg: 'A password is required'
+    };
 
-//     };
-//   }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-//   render() {
+  handleChange(event) {
+    const input = document.querySelector('input');
+    const length = input.value.length;
 
-//   }
-// }
+    if (length === 0) {
+      this.setState({
+        icon: 'fa-solid fa-x',
+        errorMsg: 'A password is required'
+      });
+    } else if (length < 8) {
+      this.setState({
+        icon: 'fa-solid fa-x',
+        errorMsg: 'Your password is too short'
+      });
+    } else {
+      this.setState({
+        icon: 'fa-solid fa-check',
+        errorMsg: ''
+      });
+    }
+  }
 
-// export default ValidatedInput;
+  render() {
+    return (
+      <div>
+        <div className='label'>Password</div>
+        <input onInput={this.handleChange} type='password' />
+        <span>
+          <i className={this.state.icon}></i>
+        </span>
+        <p className='error-msg'>{this.state.errorMsg}</p>
+      </div>
+
+    );
+  }
+}
+
+export default ValidatedInput;
