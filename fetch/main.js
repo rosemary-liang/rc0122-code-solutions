b4b41c2d835ba3991448c249791216f7a94faa7f
Rosemary Liu <rliu18@gmail.com>
2022-04-05 23:37:40 +0000

    Added catch

--- fetch/main.js
+++ fetch/main.js
@@ -1,8 +1,10 @@
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(data => console.log('jsonplaceholder data', data)
-  );
+  .then(data => console.log('jsonplaceholder data', data))
+  .catch(err => console.error('Fetch failed', err))
+;
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(response => response.json())
-  .then(data => console.log('pokemon data', data));
+  .then(data => console.log('pokemon data', data))
+  .catch(err => console.error('Fetch failed', err));
