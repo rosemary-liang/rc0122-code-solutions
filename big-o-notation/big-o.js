483f0bd88328065fe5f9efb14fce7b594c09e4ed
Rosemary Liu <rliu18@gmail.com>
2022-04-25 23:30:30 +0000

    Updated exercise.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -21,24 +21,24 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n)
-    i++                             // 2 * n = O(2n)
+    i < words.length;               // 2 * n = O(2n) ~= 0(n)
+    i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n)
+    const word = words[i];          // 2 * n = O(2n) ~= 0(n)
     let isUnique = true;            // 1 * 1 = O(1)
     for (
       let c = 0;                    // 1 * 1 = O(1)
       c < i;                        // 1 * 1 = O(1)
       c++                           // 2 * 1 = O(2)
     ) {
-      const comparing = words[c];   // 2 * n = O(2n)
-      if (comparing === word) {     // 1 * n = O(n)
-        isUnique = false;           // 1 * n = O(n)
+      const comparing = words[c];   // 2 * n^2 = O(2n^2) ~= 0(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
     if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * n = O(3n)
+      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
     }
   }
-  return unique;                    // _ * _ = O(?)
+  return unique;                    // 1 * 1 = O(1)
 } // Big O Notation for uniqueQuadratic: O(n^2)
